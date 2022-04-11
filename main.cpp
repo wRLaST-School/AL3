66e953232a7f65f80c0b0eac3027834c9b758250
WrelferAsterihimmeil <elf@wrelf.net>
2022-04-11 14:35:29 +0900

    First

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow("LE2A_21_ムラカミ_ハルヤ_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
